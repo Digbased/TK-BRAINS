c314b1d517f66c32be883d238e9f487571e5878b
Kevin Pham <kevinhp2@uci.edu>
2020-07-27 05:05:19 -0700

    Update discord-tk.py

--- discord-tk.py
+++ discord-tk.py
@@ -9,7 +9,7 @@ from minorquest import *
 from exp import *
 from babel.numbers import format_number, format_decimal, format_percent, parse_decimal
 
-TOKEN = 'NzE3OTg1MTY1MDQwODEyMDUy.Xt7TLg.3RfAn6XSNKB42kiKCUt6AWZ83EY'
+TOKEN = 'BLANK'
 
 client = discord.Client()
 bot = commands.Bot(command_prefix='!')
